48770f0c7a1c3702ffc8aa1ee63ad86c838db2ec
Victor Hugo Ferreira Marinho <victorhugoferreira452@gmail.com>
2024-06-16 12:31:34 -0300

    feat: add enum orderStage

--- src/main/java/com/victorhugo/ecommercefitness/enums/Employee/OrderStage.java
+++ src/main/java/com/victorhugo/ecommercefitness/enums/Employee/OrderStage.java
@@ -6,6 +6,7 @@ public enum OrderStage {
     CONFIRMADO("Confirmado"),
     EM_PREPARO("Em preparo"),
     EM_ROTA_DE_ENTREGA("Em rota de entrega"),
+    ENTREGUE("Entregue"),
     CANCELADO("Cancelado")    ;
 
     private final String description;
